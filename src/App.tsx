7d0d5f183864aa0b3cc58c332fc3e3bd9863001d
morisaki hirose <45589157+morisakigogogo@users.noreply.github.com>
2021-08-26 19:58:20 +0900

    add redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react'
 import './App.css'
 import { Button } from '@material-ui/core';
 import { Header, Content, Footer } from './Components/Layouts'
+import { Counter } from './features/counter/Counter'
 
 function App() {
   return (
     <Fragment> 
         <Header />
+        <Counter />
         <Content />
         <Footer />
         <Button variant="contained" color="primary">Hello material-ui</Button>
